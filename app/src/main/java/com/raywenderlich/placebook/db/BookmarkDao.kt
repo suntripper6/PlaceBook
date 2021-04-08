af95f41fabd77a304813b46b8248551656857489
Jason Bundy <jason.bundy@me.com>
2021-04-07 20:54:29 -0400

    Added stuff

---
+++ app/src/main/java/com/raywenderlich/placebook/db/BookmarkDao.kt
@@ -0,0 +1,36 @@
+package com.raywenderlich.placebook.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy.REPLACE
+import com.raywenderlich.placebook.model.Bookmark
+
+// 1 - CRUD Operations
+@Dao
+interface BookmarkDao {
+
+    // 2
+    @Query("SELECT * FROM Bookmark")
+    fun loadAll(): LiveData<List<Bookmark>>
+
+    // 3
+    @Query("SELECT * FROM Bookmark WHERE id = :bookmarkId")
+    fun loadBookmark(bookmarkId: Long): Bookmark
+
+    @Query("SELECT * FROM Bookmark WHERE id = :bookmarkId")
+    fun loadLiveBookmark(bookmarkId: Long): LiveData<Bookmark>
+
+    // 4
+    @Insert(onConflict = IGNORE)
+    fun insertBookmark(boomark: Bookmark): Long
+
+    // 5
+    @Update(onConflict = REPLACE)
+    fun updateBookmark(bookmark: Bookmark)
+
+    // 6
+    @Delete
+    fun deleteBookmark(bookmark: Bookmark)
+
+}
---
+++ app/src/main/java/com/raywenderlich/placebook/model/Bookmark.kt
@@ -0,0 +1,20 @@
+package com.raywenderlich.placebook.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// 1 DB Entity Class
+@Entity
+// 2 - primary constructor with Primary Key and fields with default values
+data class Bookmark(
+    // 3
+    @PrimaryKey(autoGenerate = true) var id: Long? = null,
+    // 4
+    var placeId: String? = null,
+    var name: String = "",
+    var address: String = "",
+    var latitude: Double = 0.0,
+    var longitude: Double = 0.0,
+    var phone: String = ""
+
+)
